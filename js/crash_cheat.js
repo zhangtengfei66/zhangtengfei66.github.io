069263e9afad15b0650cbb4544696e2a1b418539
zhangtengfei <1842650530@qq.com>
2020-01-13 10:47:30 +0800

    Site updated: 2020-01-13 10:47:28

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,18 +1,19 @@
+
 <!--崩溃欺骗-->
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         // $('[rel="icon"]').attr('href', "/image/favicon.png");
-         document.title = '(ฅ>ω<*ฅ)~来找我啊';
+         // $('[rel="favicon"]').attr('href', "/image/favicon.png");
+         document.title = '(ฅ>ω<*ฅ)~我藏好了';
          clearTimeout(titleTime);
      }
      else {
-         // $('[rel="icon"]').attr('href', "/image/favicon.png");
+         // $('[rel="favicon"]').attr('href', "/image/favicon.png");
          document.title = '(●ˇ∀ˇ●)~被你找到了';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
-             // $('[rel="icon"]').attr('href', "/image/favicon.png");
+             // $('[rel="favicon"]').attr('href', "/image/favicon.png");
          }, 2000);
      }
  });
