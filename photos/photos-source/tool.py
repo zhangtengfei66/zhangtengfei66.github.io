bc1a75cf90da3b9eb062b9f53896cf06d6da81bd
zhangtengfei <1842650530@qq.com>
2020-01-16 17:03:13 +0800

    Site updated: 2020-01-16 17:03:10

--- photos/photos-source/tool.py
+++ photos/photos-source/tool.py
@@ -131,7 +131,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("C:\\Users\\腾飞\\blog\\source\\photos\\photos-source\\photos\\data.json","w") as fp:
+    with open("C:\\Users\\腾飞\\blog\\source\\photos\\photos\\data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
@@ -173,7 +173,7 @@ def git_operation():
 if __name__ == "__main__":
     cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
     compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
-    # git_operation()    # 提交到github仓库
+    git_operation()    # 提交到github仓库
     handle_photo()     # 将文件处理成json格式，存到博客仓库中
     
     
