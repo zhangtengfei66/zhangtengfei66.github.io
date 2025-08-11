9cdb68e33141f9f055faf5306b9f8e1d641c9667
17839374848 <1842650530@qq.com>
2025-08-11 16:53:28 +0800

    Site updated: 2025-08-11 16:53:25

--- photos/photos/ins.js
+++ photos/photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/zhangtengfei66/Blog-Back-Up/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/zhangtengfei66/Blog-Back-Up/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/zhangtengfei66/zhangtengfei66.github.io/blob/master/photos/photos-source/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/zhangtengfei66/zhangtengfei66.github.io/blob/master/photos/photos-source/photos/' + data.link[i];
 
           // var minSrc = 'https://raw.githubusercontent.com/zhangtengfei66/zhangtengfei66.github.io/master/photos/photos-source/min_photos/' + data.link[i];
           // var src = 'https://raw.githubusercontent.com/zhangtengfei66/zhangtengfei66.github.io/master/photos/photos-source/photos/' + data.link[i];
